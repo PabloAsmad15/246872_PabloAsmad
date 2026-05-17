d422ab065844b0dc659e742bf3c3f06edb4efc70
PabloAsmad15 <pabloasmad15@gmail.com>
2026-05-16 20:22:13 -0500

    Parte 4

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -14,7 +14,6 @@ contract AlmacenTech246872 {
 
     Componente[] public componentes;
 
-
     modifier logEjecutor() {
         console.log("Ejecutado por: 246872 - Pablo Enrique Asmad Morgado");
         _;
@@ -27,13 +26,32 @@ contract AlmacenTech246872 {
         for (uint256 i = 0; i < componentes.length; i++) {
             require(componentes[i].id != _id, "Componente con ese ID ya existe");
         }
-
         require(_precio > 0, "El precio debe ser mayor a cero");
         componentes.push(Componente(_id, _modelo, _precio, true));
     }
 
-    
     function contarElementos() public view logEjecutor returns (uint256) {
         return componentes.length;
+    }
+
+    function inactivarElemento(uint256 _posicion) public logEjecutor {
+        require(_posicion < componentes.length, "Posicion fuera de rango");
+        componentes[_posicion].estado = false;
+    }
+
+    function pintarElementosActivos() public view logEjecutor {
+        for (uint256 i = 0; i < componentes.length; i++) {
+            if (componentes[i].estado == true) {
+                console.log("Componente activo:", componentes[i].id, componentes[i].modelo);
+            }
+        }
+    }
+
+    function pintarElementosImpares() public view logEjecutor {
+        for (uint256 i = 0; i < componentes.length; i++) {
+            if (componentes[i].id % 2 != 0) {
+                console.log("Componente impar:", componentes[i].id, componentes[i].modelo);
+            }
+        }
     }
 }
