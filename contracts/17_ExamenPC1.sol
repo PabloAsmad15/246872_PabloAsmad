d78298f7795ee0c1cf1248785f2f5e8ae2c7da6e
PabloAsmad15 <pabloasmad15@gmail.com>
2026-05-16 20:32:42 -0500

    Parte 5

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -12,7 +12,11 @@ contract AlmacenTech246872 {
         bool estado; 
     }
 
-    Componente[] public componentes;
+    uint256 public cantidad;
+
+    mapping(uint256 => Componente) public componentes;
+    
+    uint256[] private listaIds;
 
     modifier logEjecutor() {
         console.log("Ejecutado por: 246872 - Pablo Enrique Asmad Morgado");
@@ -23,34 +27,43 @@ contract AlmacenTech246872 {
     }
 
     function agregarElemento(uint256 _id, string memory _modelo, uint256 _precio) public logEjecutor {
-        for (uint256 i = 0; i < componentes.length; i++) {
-            require(componentes[i].id != _id, "Componente con ese ID ya existe");
-        }
+
+        require(componentes[_id].id == 0, "Componente con ese ID ya existe");
         require(_precio > 0, "El precio debe ser mayor a cero");
-        componentes.push(Componente(_id, _modelo, _precio, true));
+        
+
+        componentes[_id] = Componente(_id, _modelo, _precio, true);
+
+        listaIds.push(_id);
+        
+        cantidad++;
     }
+
 
     function contarElementos() public view logEjecutor returns (uint256) {
-        return componentes.length;
+        return cantidad;
     }
 
-    function inactivarElemento(uint256 _posicion) public logEjecutor {
-        require(_posicion < componentes.length, "Posicion fuera de rango");
-        componentes[_posicion].estado = false;
+
+    function inactivarElemento(uint256 _id) public logEjecutor {
+        require(componentes[_id].id != 0, "El ID introducido no existe");
+        componentes[_id].estado = false;
     }
 
     function pintarElementosActivos() public view logEjecutor {
-        for (uint256 i = 0; i < componentes.length; i++) {
-            if (componentes[i].estado == true) {
-                console.log("Componente activo:", componentes[i].id, componentes[i].modelo);
+        for (uint256 i = 0; i < listaIds.length; i++) {
+            uint256 idActual = listaIds[i];
+            if (componentes[idActual].estado == true) {
+                console.log("Componente activo:", componentes[idActual].id, componentes[idActual].modelo);
             }
         }
     }
 
     function pintarElementosImpares() public view logEjecutor {
-        for (uint256 i = 0; i < componentes.length; i++) {
-            if (componentes[i].id % 2 != 0) {
-                console.log("Componente impar:", componentes[i].id, componentes[i].modelo);
+        for (uint256 i = 0; i < listaIds.length; i++) {
+            uint256 idActual = listaIds[i];
+            if (componentes[idActual].id % 2 != 0) {
+                console.log("Componente impar:", componentes[idActual].id, componentes[idActual].modelo);
             }
         }
     }
